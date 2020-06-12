553be0547852c66ac2e29aa769239f24141abd48
Aman Agrawal <amanagrawal2098@gmail.com>
2020-06-13 01:45:03 +0530

    storage code with file transfer

--- app/src/main/java/com/example/myfirebaseapp/MainActivity.java
+++ app/src/main/java/com/example/myfirebaseapp/MainActivity.java
@@ -1,26 +1,29 @@
 package com.example.myfirebaseapp;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,11 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase m_Database;
     private DatabaseReference m_Ref;
     private String TAG="MyTag";
-    private ChildEventListener listener;
-    private RecyclerView recyclerView;
-    private UserAdapter userAdapter;
-    private List<User> list;
-    private ValueEventListener mQueryEventListener;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
 
         m_Database=FirebaseDatabase.getInstance();//this method will get the firebase whole instance;
         m_Ref=m_Database.getReference("users");//this will take up the root node of the firebase json db.
-        //here we are writing 'users' which will basically create a node inside the root instance with the same name.
+        storageReference= FirebaseStorage.getInstance().getReference("docs");//this will create the folder docs
+
         m_SendData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,98 +51,59 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        list=new ArrayList<>();
+
 
         m_ReadData.setVisibility(View.INVISIBLE);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        userAdapter=new UserAdapter(this,list);
-        recyclerView.setAdapter(userAdapter);
-        listener = new ChildEventListener() {
-
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                User user=dataSnapshot.getValue(User.class);
-                list.add(user);
-                user.setUid(dataSnapshot.getKey());
-                userAdapter.notifyDataSetChanged();//this will notfy the recycler view for any change in data
-
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-                //if we remove the data from adaptor then this function will be called
-                User user=dataSnapshot.getValue(User.class);
-                user.setUid(dataSnapshot.getKey());
-                list.remove(user);
-                userAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-//gives a failure report similar to OnFailure
-            }
-        };
-       // m_Ref.addChildEventListener(listener);
-
-        mQueryEventListener=new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                for (DataSnapshot snapshot:dataSnapshot.getChildren())
-                {
-                    User user=snapshot.getValue(User.class);
-                    list.add(user);
-                }
-                userAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        };
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        m_Ref.removeEventListener(listener);
-    }
+
 
     private void sendData() {
-        m_Ref.addValueEventListener(mQueryEventListener);//select * from db
-        //select * from db where name="abc";
-        /*
-            m_Ref.orderByChild("name").euqalTo("Ali").addValueEventListener(mQueryEventListener);
-            order by name
-            m_Ref.orderByChild("name").addValueEventListener(mQueryEventListener);
+        StorageReference child = storageReference.child("images/image.jpg");
+        Bitmap bitmap=readImage();
 
-            age>30
-            m_Ref.orderByChild("age").startAt(30).addValueEventListener(mQueryEventListener);
-            same for endig endAt
-            limit by 3
-            m_Ref.limitToFirst(2).addValue.....
-         */
-
+        ByteArrayOutputStream byteArrayOutputStream=new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
+        child.putBytes(byteArrayOutputStream.toByteArray())
+                .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(MainActivity.this, "Image inserted", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
     }
 
+    private Bitmap readImage()
+    {
+        InputStream inputStream=null;
+        try {
+            inputStream=getAssets().open("image.png");
+            BitmapDrawable bitmapDrawable= (BitmapDrawable) Drawable.createFromStream(inputStream,null );
+            return bitmapDrawable.getBitmap();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        if(inputStream!=null)
+        {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
     private void initialize() {
         m_writeData=findViewById(R.id.write_data);
         m_writeDataInt=findViewById(R.id.write_int_data);
         m_SendData=findViewById(R.id.send_data);
         m_ReadData=findViewById(R.id.read_data);
-        recyclerView=findViewById(R.id.user_recyclerview);
     }
 }
